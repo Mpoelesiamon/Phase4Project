2d618c64ac38b875dc9f3a97fbbda79b60ae6c27
Lesiamon <lesiamon.mpoe@student.moringaschool.com>
2024-02-07 15:34:55 +0300

    app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from models import db, Game
 
 from models import db
 
@@ -16,7 +17,44 @@ db.init_app(app)
 
 api = Api(app)
 
+class GameResource(Resource):
+    def get(self, game_id):
+        game = Game.query.get(game_id)
+        if game:
+            return game.serialize(), 200
+        return {'message': 'Game not found'}, 404
 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, help='Name of the game is required')
+        parser.add_argument('developer', type=str, required=True, help='Developer of the game is required')
+        args = parser.parse_args()
+
+        new_game = Game(name=args['name'], developer=args['developer'])
+        db.session.add(new_game)
+        db.session.commit()
+
+        return new_game.serialize(), 201
+
+    def patch(self, game_id):
+        game = Game.query.get(game_id)
+        if not game:
+            return {'message': 'Game not found'}, 404
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('developer', type=str)
+        args = parser.parse_args()
+
+        if 'name' in args:
+            game.name = args['name']
+        if 'developer' in args:
+            game.developer = args['developer']
+
+        db.session.commit()
+        return game.serialize(), 200
+
+api.add_resource(GameResource, '/games', '/games/<int:game_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
